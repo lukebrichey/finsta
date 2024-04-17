0f93435aad69130f5480f673eb22f5b43524f406
Your Namecd <you@example.com>
2024-04-16 21:34:39 -0400

    hi

--- src/app/feeds/page.tsx
+++ src/app/feeds/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import SidePanel from '../../components/SidePanel'; // Assuming the SidePanel component is in the same directory
 import { PlaylistCard } from '../../components/feedcard'; // Assuming the PlaylistCard component is in the same directory
+import image1 from './snow.png'; // Import the image files
+
 
 const Page = () => {
   // Sample data for playlists
   const playlists = [
-    { id: 1, imageUrl: 'url_to_image_1', label: 'Playlist 1' },
-    { id: 2, imageUrl: 'url_to_image_2', label: 'Playlist 2' },
-    { id: 3, imageUrl: 'url_to_image_3', label: 'Playlist 3' },
+    { id: 1, imageUrl: image1, label: 'NFL Highlights' },
+    { id: 2, imageUrl: 'url_to_image_2', label: 'England Rugby' },
+    { id: 3, imageUrl: 'url_to_image_3', label: 'Study Motivation' },
     // Add more playlists as needed
   ];
 
