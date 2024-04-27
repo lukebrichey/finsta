be888aa84b1117716617de7db67fd8b836040e86
Your Namecd <you@example.com>
2024-04-27 10:58:56 -0400

    edit

--- src/app/activity/page.tsx
+++ src/app/activity/page.tsx
@@ -1,12 +1,10 @@
+// Page.tsx
 import React from 'react';
 import CarouselSpacing from '../../components/carousel';
 
 const Page = () => {
   return (
-    <div className="flex flex-col mt-8">
-      <div className="ml-4 mb-4">
-        <h1 className="text-white font-bold text-3xl">Activity</h1>
-      </div>
+    <div className="flex flex-col items-center mt-8">
       <div className="flex justify-center mb-4">
         <h2 className="text-white font-bold text-xl">Friend Requests</h2>
       </div>
